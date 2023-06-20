be5d59cac3214d01731142060eb334fe68a25947
Marka <khalilovakram07@gmail.com>
2023-06-20 14:24:32 +0600

    add project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+
+  // http://localhost:3002/presons
+
+  const [name, setName] = useState('')
+  const [number, setNumber] = useState('')
+  const [searchText, setSearchText] = useState('')
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+    axios.get("http://localhost:3002/persons")
+      .then(res => setPersons(res.data))
+  }, [])
+
+  const addContact = (e) => {
+
+    e.preventDefault()
+
+    const newContact = {
+      name: name,
+      number: number
+    }
+
+    axios.post("http://localhost:3002/persons", newContact)
+      .then(res => {
+        setName('')
+        setNumber('')
+        setPersons(persons.concat(res.data))
+      })
+  }
+
+  // concat() - это метод массива который не работает с оригиналом, а работает с копией
+
+  // persons - это состояние, setPersons - это функция которая изменяет наше состояние
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Контактная книга</h2>
+      <form onSubmit={addContact}>
+
+        <div>
+
+          <input
+            type="text"
+            value={name}
+            placeholder="Enter name..."
+            onChange={(e) => setName(e.target.value)}
+          />
+
+          <input
+            type="phone"
+            value={number}
+            placeholder="Enter phone-number..."
+            onChange={(e) => setNumber(e.target.value)}
+          />
+
+          <input 
+          type="text" 
+          placeholder='Search...'
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+          />
+
+        </div>
+
+        <div>
+          <button type="submit">Add contact</button>
+        </div>
+
+      </form>
+
+      {
+      persons.map(person => {
+        return (
+          <div key={person.id} style={{ border: "2px solid khaki", margin: "10px 0" }}>
+            <h2>Name: {person.name}</h2>
+            <h2>Tel: {person.number}</h2>
+          </div>
+        )
+      })}
+
     </div>
   );
 }
